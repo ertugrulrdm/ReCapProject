f6c84d2405b55fc5934bc0c5f8882294fc3c474d
Ertugrul Emer <ertugrul4653@hotmail.com>
2021-02-06 18:06:26 +0300

    06.02.2021 Derse Hazır ve Nazır.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,33 +12,12 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
-
-            Car newCar = new Car();
-            newCar.Id = 5;
-            newCar.BrandId = 2;
-            newCar.ColorId = 3;
-            newCar.ModelYear = 2017;
-            newCar.DailyPrice = 250;
-            newCar.Description = "2017 model siyah Fiat Egea";
-
             EfCarDal efCarDal = new EfCarDal();
-            
-
             var cars = carManager.GetAll();
             foreach (var car in cars)
             {
                 Console.WriteLine("-------------");
-                Console.WriteLine("Arabanın;\nID'si: {0}\nMarka ID'si: {1}\nRenk ID'si: {2}\nYılı: {3}\nGünlük Ücreti: {4}\nAçıklaması: {5}", car.Id, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Description);
-                Console.WriteLine("-------------");
-            }
-
-            efCarDal.Delete(newCar);
-
-            Console.WriteLine("--------------------------------------------------");
-            foreach (var car in cars)
-            {
-                Console.WriteLine("-------------");
-                Console.WriteLine("Arabanın;\nID'si: {0}\nMarka ID'si: {1}\nRenk ID'si: {2}\nYılı: {3}\nGünlük Ücreti: {4}\nAçıklaması: {5}", car.Id, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Description);
+                Console.WriteLine("Arabanın;\nID'si: {0}\nMarka ID'si: {1}\nRenk ID'si: {2}\nYılı: {3}\nGünlük Ücreti: {4} TL\nAçıklaması: {5}", car.Id, car.BrandId, car.ColorId, car.ModelYear, car.DailyPrice, car.Description);
                 Console.WriteLine("-------------");
             }
         }
