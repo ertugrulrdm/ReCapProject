451e976efb56fe3cab354e684cc0f523564c3a13
Ertugrul Emer <ertugrul4653@hotmail.com>
2021-02-06 17:05:46 +0300

    06.02.21 Derse Hazır.

---
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -0,0 +1,65 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfColorDal : IColorDal
+    {
+        public void Add(Color entity)
+        {
+            using (MyDbContext context = new MyDbContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Color entity)
+        {
+            using (MyDbContext context = new MyDbContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Color Get(Expression<Func<Color, bool>> filter)
+        {
+            using (MyDbContext context = new MyDbContext())
+            {
+                return context.Set<Color>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            
+        }
+
+        public List<Color> GetAll(Expression<Func<Color, bool>> filter = null)
+        {
+            using (MyDbContext context = new MyDbContext())
+            {
+                return filter == null ? context.Set<Car>().ToList() : context.Set<Car>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Color entity)
+        {
+            using (MyDbContext context = new MyDbContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
